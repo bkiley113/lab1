4303cdf8ca74fac8297a53da4c74fb1b27fb764c
Brandon Kiley <b.kiley.113@gmail.com>
2025-01-25 15:17:59 -0800

    message

--- pipe.c
+++ pipe.c
@@ -10,6 +10,10 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Usage: %s  <command>\n", argv[0]);
 		return 1;
 	}
+	if(argc > 2){
+		fprintf(stdout, "I haven't figured this out yet lol");
+		return 1;
+	}
 
 	execlp(argv[1], argv[1], NULL);
 	perror("execlp");
