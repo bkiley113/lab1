fb25396152bbb9ccad553ea8a27c9d4fd70f4c6c
Brandon Kiley <b.kiley.113@gmail.com>
2025-01-26 12:43:52 -0800

    subtask 3, I/O redirrection for a single command line argument with arbitrary additional arguments

--- pipe.c
+++ pipe.c
@@ -11,12 +11,61 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	char **command_args = &argv[1];
-	command_args[argc - 1] = NULL;
+	int pipefd[2];
+	pid_t pid;
 
-	execvp(argv[1], command_args);
-	perror("execvp");
+	//create a pipe and check for errors
+	if (pipe(pipefd) < 0){
+		perror("pipe");
+		exit(1);
+	}
 
-	return 1;
+	//create new process and check for errors
+	pid = fork();
+	if(pid < 0){
+		perror("fork");
+		exit(1);
+	}
+
+	//child process
+	if(pid == 0){
+		//we want child process to run command so close read end of pipe
+		close(pipefd[0]);
+		//any stdout of the process redirected to write end of pipe
+		dup2(pipefd[1], STDOUT_FILENO);
+		//now STDOUT points to the write end of pipe, we can close it to avoid memory leak
+		close(pipefd[1]);
+
+		//here we create a subarray of the cmd line arguments
+		char **command_args = &argv[1];
+		//make sure it is null-terminated so execvp works
+		command_args[argc-1] = NULL;
+
+		//replace process image with cmd line program
+		execvp(argv[1], command_args);
+
+		//it should no longer execute this code but it will if there is an error
+		perror("execvp");
+		exit(1);
+
+	}
+	//parent process, want to read from child process
+	else{
+		close(pipefd[1]);
+
+		char buffer[1024];
+		ssize_t nbytes;
+
+		while((nbytes = read(pipefd[0], buffer, sizeof(buffer) - 1)) > 0){
+			buffer[nbytes] = '/0';
+			printf("Parent received: %s", buffer);
+		}
+
+		close(pipefd[0]);
+
+		wait(NULL);
+	}
+
+
 }
 
