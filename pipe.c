e610e24d483868c828cd1cef35fe26d2dd192f84
Brandon Kiley <b.kiley.113@gmail.com>
2025-01-31 18:16:06 -0800

    testing out command filtering from argv

--- pipe.c
+++ pipe.c
@@ -3,79 +3,139 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
 
-int main(int argc, char *argv[])
-{
-	if(argc < 2){
-		fprintf(stderr, "Usage: %s  <command>\n", argv[0]);
-		return 1;
+//I know that it is 4096 on Linux, but putting myself in the portability mindset...
+//...it is probably good practice to not make more conservative assumptions
+#define MAX_PATH_LEN 1024
+
+int is_executable(const char *cmd){
+	if(!cmd || strlen(cmd) == 0); return 0;
+
+	//if they give us a relative path
+	if(strchr(cmd, '/')){
+		return (access(cmd, X_OK) == 0);
 	}
 
-	int pipefd[2];
-	pid_t pid;
+	//now we begin our search of the PATH for executables
+	char *path_env = getenv("PATH");
+	
+	//if there isnt one, say so!
+	if(!path_env || strlen(path_env) == 0) return 0;
 
-	//create a pipe and check for errors
-	if (pipe(pipefd) < 0){
-		perror("pipe");
-		exit(1);
-	}
+	//to safely handle out path string, make a copy
+	char path_copy[MAX_PATH_LEN];
+	strncpy(path_copy, path_env, MAX_PATH_LEN -1);
+	//null terminate it
+	path_copy[MAX_PATH_LEN - 1] = '\0';
 
-	//create new process and check for errors
-	pid = fork();
-	if(pid < 0){
-		perror("fork");
-		exit(1);
-	}
+	char *saveptr;
+	char *directory = strtok_r(path_copy, ":", &saveptr);
 
-	//child process
-	if(pid == 0){
-		//we want child process to run command so close read end of pipe
-		close(pipefd[0]);
-		//any stdout of the process redirected to write end of pipe
-		if(dup2(pipefd[1], STDOUT_FILENO) < 0){
-			perror("dup2");
-			exit(1);
+	char full_path[MAX_PATH_LEN];
+
+	while(directory){
+		snprintf(full_path, sizeof(full_path), "%s/%s", directory, cmd);
+		if (access(full_path, X_OK ==0)){
+			return 1;
 		}
-		//now STDOUT points to the write end of pipe, we can close it to avoid memory leak
-		close(pipefd[1]);
-
-		//here we create a subarray of the cmd line arguments
-		char **command_args = &argv[1];
-		//make sure it is null-terminated so execvp works
-		command_args[argc-1] = NULL;
-
-		//replace process image with cmd line program
-		execvp(argv[1], command_args);
-
-		//it should no longer execute this code but it will if there is an error
-		perror("execvp");
-		exit(1);
-
+		directory = strtok_r(NULL, ":", &saveptr);
 	}
-	//parent process, want to read from child process
-	else{
-		close(pipefd[1]);
 
-		//use dup2 to redirect read end of pipe (child stdout) to parent stdin
-		if(dup2(pipefd[0], STDIN_FILENO) < 0){
-			perror("dup2");
-			exit(1);
-		}
-		close(pipefd[0]);
-		//initialize a buffer to read parent stdin from pipe into
-		char buffer[1024];
+	return 0;
 
-		printf("Parent is reading child's stdout to its stdin via pipe: \n");
+}
+
+// int main(int argc, char *argv[])
+// {
+// 	if(argc < 2){
+// 		fprintf(stderr, "Usage: %s  <command>\n", argv[0]);
+// 		return 1;
+// 	}
+
+// 	int pipefd1[2];
+// 	pid_t pid;
+
+// 	//create a pipe and check for errors
+// 	if (pipe(pipefd1) < 0){
+// 		perror("pipe");
+// 		exit(1);
+// 	}
+
+// 	//create new process and check for errors
+// 	pid = fork();
+// 	if(pid < 0){
+// 		perror("fork");
+// 		exit(1);
+// 	}
+
+// 	//child process
+// 	if(pid == 0){
+// 		//we want child process to run command so close read end of pipe
+// 		close(pipefd1[0]);
+// 		//any stdout of the process redirected to write end of pipe
+// 		if(dup2(pipefd1[1], STDOUT_FILENO) < 0){
+// 			perror("dup2");
+// 			exit(1);
+// 		}
+// 		//now STDOUT points to the write end of pipe, we can close it to avoid memory leak
+// 		close(pipefd1[1]);
+
+// 		//here we create a subarray of the cmd line arguments
+// 		char **command_args = &argv[1];
+// 		//make sure it is null-terminated so execvp works, use pointer arithmetic
+// 		command_args[argc-1] = NULL;
+
+// 		//replace process image with cmd line program given by argv
+// 		execvp(argv[1], command_args);
+
+// 		//it should no longer execute this code but it will if there is an error
+// 		perror("execvp");
+// 		exit(1);
+
+// 	}
+// 	//parent process, want to read from child process
+// 	else{
+// 		close(pipefd1[1]);
+
+// 		//use dup2 to redirect read end of pipe (child stdout) to parent stdin
+// 		if(dup2(pipefd1[0], STDIN_FILENO) < 0){
+// 			perror("dup2");
+// 			exit(1);
+// 		}
+// 		close(pipefd1[0]);
+// 		//initialize a buffer to read parent stdin from pipe into
+// 		char buffer[1024];
+
+// 		printf("Parent is reading child's stdout to its stdin via pipe: \n");
 		
-		//read from stdin into buffer, while it is not NULL we can print it!
-		while(fgets(buffer, sizeof(buffer), stdin) != NULL){
-			//printf directly to stdout of parent process
-			printf("%s", buffer);
-		}
+// 		//read from stdin into buffer, while it is not NULL we can print it!
+// 		while(fgets(buffer, sizeof(buffer), stdin) != NULL){
+// 			//printf directly to stdout of parent process
+// 			printf("%s", buffer);
+// 		}
 
-		wait(NULL);
-	}
+// 		wait(NULL);
+// 	}
 
 
+// }
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <command1> <command2> ...\n", argv[0]);
+        return 1;
+    }
+
+    printf("Classification of arguments:\n");
+    for (int i = 1; i < argc; i++) {
+        if (is_executable(argv[i])) {
+            printf("  ✅ '%s' is a command.\n", argv[i]);
+        } else {
+            printf("  🔹 '%s' is likely an argument.\n", argv[i]);
+        }
+    }
+
+    return 0;
 }
 
