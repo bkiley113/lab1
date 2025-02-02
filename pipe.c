d97b1854882ce7f1772dc50345bccef46e08810c
Brandon Kiley <b.kiley.113@gmail.com>
2025-02-01 23:27:37 -0800

    more fixes

--- pipe.c
+++ pipe.c
@@ -48,37 +48,37 @@ int is_executable(const char *cmd) {
     return 0;  //no bueno
 }
 
-char ***parse_commands(int argc, char *argv[], int *cmd_count){
+char ***parse_commands(int argc, char *argv[], int *cmd_count) {
     char ***commands = malloc(MAX_COMMANDS * sizeof(char **));
-    if (!commands){
+    if (!commands) {
         perror("malloc failure");
         exit(1);
     }
 
     int cmd_index = -1;
     int arg_index = 0;
-    for (int i = 1; i < argc; i++){
-        if (is_executable(argv[i])){
-            if(cmd_index >=0){
+    for (int i = 1; i < argc; i++) {
+        if (is_executable(argv[i])) {
+            if (cmd_index >= 0) {
                 commands[cmd_index][arg_index] = NULL;
             }
             cmd_index++;
             arg_index = 0;
-            commands[cmd_index] = malloc(MAX_COMMANDS * sizeof(char *));
-            if(!commands[cmd_index]){
+            commands[cmd_index] = malloc((argc - i + 1) * sizeof(char *));  // Allocate only required space
+            if (!commands[cmd_index]) {
                 perror("malloc failed");
                 exit(1);
             }
         }
-        if(cmd_index >=0){
+        if (cmd_index >= 0) {
             commands[cmd_index][arg_index++] = argv[i];
         }
     }
-    if(cmd_index >=0){
+    if (cmd_index >= 0 && arg_index > 0) {
         commands[cmd_index][arg_index] = NULL;
     }
 
-    *cmd_count = cmd_index +1;
+    *cmd_count = cmd_index + 1;
     return commands;
 }
 
@@ -100,19 +100,15 @@ void free_commands(char ***commands, int cmd_count) {
 }
 
 void execute_pipeline(char ***commands, int cmd_count) {
-    int pipes[cmd_count - 1][2];  // Create pipes for communication
+    int pipes[cmd_count - 1][2];  
     pid_t pids[cmd_count];
 
     for (int i = 0; i < cmd_count; i++) {
-        // Create pipe except for the last command
-        if (i < cmd_count - 1) {
-            if (pipe(pipes[i]) == -1) {
-                perror("pipe");
-                exit(1);
-            }
+        if (i < cmd_count - 1 && pipe(pipes[i]) == -1) {
+            perror("pipe");
+            exit(1);
         }
 
-        // Fork process
         pids[i] = fork();
         if (pids[i] < 0) {
             perror("fork");
@@ -120,38 +116,29 @@ void execute_pipeline(char ***commands, int cmd_count) {
         }
 
         if (pids[i] == 0) {  // Child process
-            if (i > 0) { // If not first command, read from previous pipe
+            if (i > 0) { 
                 dup2(pipes[i - 1][0], STDIN_FILENO);
-                close(pipes[i - 1][0]);
-                close(pipes[i - 1][1]);
             }
-
-            if (i < cmd_count - 1) { // If not last command, write to next pipe
+            if (i < cmd_count - 1) { 
                 dup2(pipes[i][1], STDOUT_FILENO);
-                close(pipes[i][0]);
-                close(pipes[i][1]);
             }
 
-            // Close all other pipes
-            for (int j = 0; j < i - 1; j++) {
+            for (int j = 0; j < cmd_count - 1; j++) {
                 close(pipes[j][0]);
                 close(pipes[j][1]);
             }
 
-            // Execute command
             execvp(commands[i][0], commands[i]);
             perror("execvp");
-            exit(1);
-        }
-
-        // Parent closes unused pipe ends
-        if (i > 0) {
-            close(pipes[i - 1][0]);
-            close(pipes[i - 1][1]);
+            exit(EXIT_FAILURE);
         }
     }
 
-    // Wait for all child processes
+    for (int i = 0; i < cmd_count - 1; i++) {
+        close(pipes[i][0]);
+        close(pipes[i][1]);
+    }
+
     for (int i = 0; i < cmd_count; i++) {
         waitpid(pids[i], NULL, 0);
     }
@@ -166,7 +153,15 @@ int main(int argc, char *argv[]) {
     int cmd_count;
     char ***commands = parse_commands(argc, argv, &cmd_count);
 
+    if (cmd_count == 0) {
+        fprintf(stderr, "Error: No valid executable commands found.\n");
+        free(commands);
+        return 1;
+    }
+
+    print_commands(commands, cmd_count);
     execute_pipeline(commands, cmd_count);
+    free_commands(commands, cmd_count);
 
     return 0;
 }
