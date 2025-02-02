8b4649e23efb43acc634ea3d66bab5c3a387fadb
Brandon Kiley <b.kiley.113@gmail.com>
2025-02-01 23:36:33 -0800

    more fix

--- pipe.c
+++ pipe.c
@@ -5,47 +5,39 @@
 #include <sys/wait.h>
 #include <string.h>
 
-//I know that it is 4096 on Linux, but putting myself in the portability mindset...
-//...it is probably good practice to not make more conservative assumptions
 #define MAX_PATH_LEN 1024
 #define MAX_COMMANDS 16
 
 int is_executable(const char *cmd) {
     if (!cmd || strlen(cmd) == 0) return 0;
 
-    //if they give us a path of any kind, we can check it directly
     if (strchr(cmd, '/')) {
         return (access(cmd, X_OK) == 0);
     }
 
-    // if not, we will search for an executable in the environment variable
     char *path_env = getenv("PATH");
     if (!path_env || strlen(path_env) == 0) {
         return 0;
     }
 
-    //we make a copy of path so we don't accidentally change the original
     char path_copy[MAX_PATH_LEN];
     strncpy(path_copy, path_env, MAX_PATH_LEN - 1);
     path_copy[MAX_PATH_LEN - 1] = '\0';
 
-    // we can use strtok to tokenize the path, going one directory at a time
     char *saveptr;
     char *directory = strtok_r(path_copy, ":", &saveptr);
 
-	//here is the buffer that will hold each path tested
     char full_path[MAX_PATH_LEN];
 
     while (directory) {
         snprintf(full_path, sizeof(full_path), "%s/%s", directory, cmd);
         if (access(full_path, X_OK) == 0) {
-            return 1;  //found executable!!!
+            return 1;
         }
-		//have strtok_r pick up where it left off
         directory = strtok_r(NULL, ":", &saveptr);
     }
 
-    return 0;  //no bueno
+    return 0;
 }
 
 char ***parse_commands(int argc, char *argv[], int *cmd_count) {
@@ -64,7 +56,7 @@ char ***parse_commands(int argc, char *argv[], int *cmd_count) {
             }
             cmd_index++;
             arg_index = 0;
-            commands[cmd_index] = malloc((argc - i + 1) * sizeof(char *));  // Allocate only required space
+            commands[cmd_index] = malloc((argc - i + 1) * sizeof(char *));
             if (!commands[cmd_index]) {
                 perror("malloc failed");
                 exit(1);
@@ -100,7 +92,7 @@ void free_commands(char ***commands, int cmd_count) {
 }
 
 void execute_pipeline(char ***commands, int cmd_count) {
-    int pipes[cmd_count - 1][2];  
+    int pipes[cmd_count - 1][2];
     pid_t pids[cmd_count];
 
     for (int i = 0; i < cmd_count; i++) {
@@ -115,11 +107,11 @@ void execute_pipeline(char ***commands, int cmd_count) {
             exit(1);
         }
 
-        if (pids[i] == 0) {  // Child process
-            if (i > 0) { 
+        if (pids[i] == 0) {
+            if (i > 0) {
                 dup2(pipes[i - 1][0], STDIN_FILENO);
             }
-            if (i < cmd_count - 1) { 
+            if (i < cmd_count - 1) {
                 dup2(pipes[i][1], STDOUT_FILENO);
             }
 
